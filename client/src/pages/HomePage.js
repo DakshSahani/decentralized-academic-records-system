90a912aa2bd1fe7a7d3689278dfa545821f695ec
Dev-Sahani <devdaksh8102003@gmail.com>
2024-02-09 21:24:06 +0530

    Operations images changed

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,18 +4,18 @@ export default function HomePage({wallet}) {
   return (
     <main className="w-full h-full flex justify-evenly items-center">
       <Link 
-        className="p-10 border border-black rounded-2xl flex flex-col justify-around gap-8 hover:cursor-pointer"
+        className="p-10 border border-black rounded-2xl flex flex-col justify-around items-center gap-8 hover:cursor-pointer transform transition-transform duration-300 hover:scale-[1.03]"
         to="/view"
       >
-          <img src="./view.png" alt="upload" />
+          <img className="w-[20vw]" src="./view.svg" alt="upload" />
           <h3>View Records</h3>
       </Link>
 
       <Link 
-        className="p-10 border border-black rounded-2xl flex flex-col justify-around gap-8 hover:cursor-pointer"
+        className="p-10 border border-black rounded-2xl flex flex-col justify-around items-center gap-8 hover:cursor-pointer transform transition-transform duration-300 hover:scale-[1.03]"
         to="/upload"
       >
-          <img src="./upload.png" alt="upload" />
+          <img className="w-[20vw] my-8" src="./upload.svg" alt="upload" />
           <h3>Upload Records</h3>
       </Link>
     </main>
