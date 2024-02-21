9526a096a8373a08a86609517484073044264d68
DakshSahani <dakshsahani8123@gmail.com>
2024-02-21 21:40:15 +0530

    only admin logic change

--- client/src/components/ConnectModal.jsx
+++ client/src/components/ConnectModal.jsx
@@ -3,7 +3,7 @@ import { Input, Modal } from "antd";
 import { Wallet, validateMnemonic, VoyageProvider } from "js-moi-sdk";
 import { toastError } from "../utils/toastWrapper";
 import { useAppContext } from "../context/Context";
-import logic from "../interface/logic";
+// import logic from "../interface/logic";
 
 const provider = new VoyageProvider("babylon");
 const account = "m/44'/6174'/7020'/0/0"; // 0th account path derivation
@@ -12,11 +12,11 @@ const ConnectModal = ({ isModalOpen, showConnectModal }) => {
   const { setWallet } = useAppContext();
   const [mnemonic, setMnemonic] = useState("");
   const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
 
   const handleConnect = async (mnemonic) => {
-    if(loading) return
-    setLoading(true)
+    // if(loading) return
+    // setLoading(true)
     try {
       if (!validateMnemonic(mnemonic)) {
         return setError("Incorrect mnemonic");
@@ -24,19 +24,20 @@ const ConnectModal = ({ isModalOpen, showConnectModal }) => {
 
       const wallet = new Wallet(provider);
       await wallet.fromMnemonic(mnemonic, account);
-      const res = await logic.isAdmin(wallet)
-      if(res){
-        setWallet(wallet, true);
-      }else{
-        toastError("The Mnemonic does not match")
-      }
+      // const res = await logic.isAdmin(wallet)
+      // if(res){
+      setWallet(wallet, true);
+      // }else{
+      //   toastError("The Mnemonic does not match")
+      // }
       setError("");
       showConnectModal(false);
     } catch (error) {
       toastError(error.message);
-    }finally{
-      setLoading(false)
     }
+    // finally{
+    //   setLoading(false)
+    // }
   };
 
   const handleCancel = () => {
@@ -53,7 +54,7 @@ const ConnectModal = ({ isModalOpen, showConnectModal }) => {
         onCancel={handleCancel}
         destroyOnClose={true}
       >
-        {loading && <p style={{ color: "red"}}>loading...</p>}
+        {/* {loading && <p style={{ color: "red"}}>loading...</p>} */}
         <Input
           style={{ border: "1px solid black" }}
           onChange={(e) => setMnemonic(e.target.value)}
