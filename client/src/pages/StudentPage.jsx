eecff7214750ef22e45e004406fbb8a73fa4ea6e
Dev-Sahani <devdaksh8102003@gmail.com>
2024-02-25 12:39:46 +0530

    some ui refractored

--- client/src/pages/StudentPage.jsx
+++ client/src/pages/StudentPage.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import {useAppContext} from "../context/Context"
-import { Loader } from "../components";
+import { LoadingWrapper } from "../components";
 import CourseTable from "./CoursesTable"
 import NotFoundPage from "./NotFoundPage";
 import AddCourseForm from "./AddCourseForm";
+import WentWrongPage from "./WentWrongPage"
 import { motion } from "framer-motion";
 import ProtectedPage from "./ProtectedPage";
 
 export default function StudentPage() {
   const { studentId: id } = useParams();
-  const { records, loading, wallet } = useAppContext()
+  const { records, wallet } = useAppContext()
   
   const [student ,setStudent] = useState({});
   const [showAddCourseForm, setShowAddCourseForm] = useState(false);
@@ -20,8 +21,7 @@ export default function StudentPage() {
       // eslint-disable-next-line
   },[records])
     
-  if(!records) return <div>Something Went Wrong</div>;
-  if(loading || records === undefined) return <Loader loading={loading} color="black" size="2rem"/>
+  if(!records) return <WentWrongPage />
   if(!student) return <NotFoundPage />
 
   const variants = {
@@ -29,30 +29,32 @@ export default function StudentPage() {
     visible: {width: "30vw", height: "100%", opacity: 1},
   }
   return (
+    <LoadingWrapper>
     <ProtectedPage condition={!showAddCourseForm || wallet}>
-    <div className={`w-full px-[2rem] flex flex-col-reverse lg:flex-row items-center lg:justify-between ${showAddCourseForm ? "lg:gap-24": ""}`}>
-        <motion.div
-            className={`${showAddCourseForm ? "block" : "hidden"} p-4 flex items-center justify-center lg:justify-start`}
-            variants={variants}
-            initial="invisible"
-            animate={ showAddCourseForm ? "visible" : "invisible" }
-        >
-            <AddCourseForm student={student} setShowAddCourseForm={setShowAddCourseForm} />
-        </motion.div>
+        <div className={`w-full px-[2rem] flex flex-col-reverse lg:flex-row items-center lg:justify-between ${showAddCourseForm ? "lg:gap-24": ""}`}>
+            <motion.div
+                className={`${showAddCourseForm ? "block" : "hidden"} p-4 flex items-center justify-center lg:justify-start`}
+                variants={variants}
+                initial="invisible"
+                animate={ showAddCourseForm ? "visible" : "invisible" }
+            >
+                <AddCourseForm student={student} setShowAddCourseForm={setShowAddCourseForm} />
+            </motion.div>
 
-        <div className="w-full p-8 mb-4 flex flex-col items-center bg-gray-50 shadow-md">
-            <CourseTable />
-                {
-                    !showAddCourseForm && 
-                    <button 
-                        className="bg-primary text-white rounded-lg p-2 mt-8"
-                        onClick={()=>setShowAddCourseForm(!showAddCourseForm)}
-                    > 
-                        Add Course
-                    </button>
-                }
+            <div className="w-full p-8 mb-4 flex flex-col items-center bg-gray-50 shadow-md">
+                <CourseTable />
+                    {
+                        !showAddCourseForm && 
+                        <button 
+                            className="bg-primary text-white rounded-lg p-2 mt-8"
+                            onClick={()=>setShowAddCourseForm(!showAddCourseForm)}
+                        > 
+                            Add Course
+                        </button>
+                    }
+            </div>
         </div>
-    </div>
-    </ProtectedPage>
+        </ProtectedPage>
+    </LoadingWrapper>
   );
 }
--- client/src/pages/UploadPage.jsx
+++ client/src/pages/UploadPage.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react"
-import { ListComponent, Loader, SearchFilter } from "../components"
+import { ListComponent, LoadingWrapper, SearchFilter } from "../components"
 import { useAppContext } from "../context/Context"
 import { Link } from "react-router-dom";
 
 export default function UploadPage() {
-  let { loading, records } = useAppContext();
+  let { records } = useAppContext();
   const [localRecords, setLocalRecords] = useState(records)
   
   useEffect(()=>{
@@ -19,53 +19,47 @@ export default function UploadPage() {
   }
   
   return (
-    <div className="w-full flex flex-col justify-between px-[2rem] relative">
-      {
-        loading
-        ?
-        <div className="w-[80%] mt-8 w-full flex justify-center items-center">
-          <Loader loading={loading} size={"2rem"} color="black"/>
-        </div>
-        :        
-        <div className="flex justify-between items-start">
-
-          <div className="hidden h-full md:block sticky left-0 top-[22vh] flex flex-col gap-16 border rounded-3xl border-black p-10">
-            <img className="md:w-[20vw] lg:w-[25vw] mb-16" src="/select-student.svg" alt="Add Student" />
-            <Link to="/records/add" className="w-full bg-primary p-2 px-2.5 rounded-lg text-white text-center hover:scale-[1.04]">
-              Add student +
-            </Link>
-          </div>
-
-          <div className="flex flex-col gap-2 basis-[60%]">
-            <div className="w-full p-2 flex justify-between items-center gap-2 bg-primary border-primary rounded-xl text-white ">
-              <img src="/search.svg" className="w-8 h-8 mr-2 bg-primary rounded-l-xl" alt="search" />
-              <form className="w-full p-1">
-                <SearchFilter 
-                  classNames="w-full"
-                  searchFunc={search} 
-                  labels={["Student Name", "Student ID"]}
-                  names={["studentName", "studentId"]}
-                />
-              </form>
+    <LoadingWrapper>
+      <div className="w-full flex flex-col justify-between px-[2rem] relative">    
+          <div className="flex justify-between items-start">
+    
+            <div className="hidden h-full md:block sticky left-0 top-[22vh] flex flex-col gap-16 border rounded-3xl border-black p-10">
+              <img className="md:w-[20vw] lg:w-[25vw] mb-16" src="/select-student.svg" alt="Add Student" />
+              <Link to="/records/add" className="w-full bg-primary p-2 px-2.5 rounded-lg text-white text-center hover:scale-[1.04]">
+                Add student +
+              </Link>
             </div>
-
-            <ul className="mt-4">
-              {
-                localRecords?.map((student, indx)=>
-                  <ListComponent 
-                    heading={student.studentName}
-                    id={student.studentId}
-                    key={indx} 
-                    onClick={()=>{}}
-                    to={`/records/${student.studentId}`}
+    
+            <div className="flex flex-col gap-2 basis-[60%]">
+              <div className="w-full p-2 flex justify-between items-center gap-2 bg-primary border-primary rounded-xl text-white ">
+                <img src="/search.svg" className="w-8 h-8 mr-2 bg-primary rounded-l-xl" alt="search" />
+                <form className="w-full p-1">
+                  <SearchFilter 
+                    classNames="w-full"
+                    searchFunc={search} 
+                    labels={["Student Name", "Student ID"]}
+                    names={["studentName", "studentId"]}
                   />
-                )
-              }
-            </ul>
+                </form>
+              </div>
+    
+              <ul className="mt-4">
+                {
+                  localRecords?.map((student, indx)=>
+                    <ListComponent 
+                      heading={student.studentName}
+                      id={student.studentId}
+                      key={indx} 
+                      onClick={()=>{}}
+                      to={`/records/${student.studentId}`}
+                    />
+                  )
+                }
+              </ul>
+            </div>
+              
           </div>
-          
-        </div>
-      }
-    </div>
-  )
+      </div>
+    </LoadingWrapper>
+  );
 }
